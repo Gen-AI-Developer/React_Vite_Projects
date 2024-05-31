4a4e3bdc184f4a12188de83500dd58fbba5f7e49
Syed Safdar Ali Shah <116350058+Gen-AI-Developer@users.noreply.github.com>
2024-05-31 17:16:23 +0500

    productsArrayDone

--- BasicViteProject/src/components/AdminPanel.tsx
+++ BasicViteProject/src/components/AdminPanel.tsx
@@ -1,8 +1,21 @@
 
 const AdminPanel = () => {
+    const products = [
+        { title: 'Cabbage', id: 1 },
+        { title: 'Garlic', id: 2 },
+        { title: 'Apple', id: 3 },
+    ];
+
     return (
-        <div><h1>Admin Panel</h1></div>
-    )
+        <div>
+            <h1>Products</h1>
+            {products.map((products) => {
+                return <div>
+                    {products.id} - {products.title}
+                </div>
+            })}
+        </div>
+    );
 }
 
-export default AdminPanel
+export default AdminPanel;
