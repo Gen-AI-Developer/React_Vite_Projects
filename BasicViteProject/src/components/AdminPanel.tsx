9bfdec494dc8dfbe0fec1794f28132a944129cc1
Syed Safdar Ali Shah <116350058+Gen-AI-Developer@users.noreply.github.com>
2024-05-31 21:02:26 +0500

    updated

--- BasicViteProject/src/components/AdminPanel.tsx
+++ BasicViteProject/src/components/AdminPanel.tsx
@@ -1,22 +1,35 @@
-
+import { useState } from 'react';
 const AdminPanel = () => {
-    const products = [
-        { title: 'Cabbage', id: 1 },
-        { title: 'Garlic', id: 2 },
-        { title: 'Apple', id: 3 },
-    ];
+    const [count, setCount] = useState(0);
+
+    function handleClick() {
+        setCount(count + 1)
+    }
+
+    // const products = [
+    //     { title: 'Cabbage', id: 1 },
+    //     { title: 'Garlic', id: 2 },
+    //     { title: 'Apple', id: 3 },
+    // ];
 
     return (
         <div>
-            <h1>Products</h1>
-            {products.map((product) => {
-                return <div key={product.id}>
-                    <li>
-                        {product.title}
-                    </li>
-                </div>
-            })}
+            <button onClick={handleClick}>
+                This is a button
+            </button>
+            <h1>Counter : {count}</h1>
         </div>
+
+        // <div>
+        //     <h1>Products</h1>
+        //     {products.map((product) => {
+        //         return <div key={product.id}>
+        //             <li>
+        //                 {product.title}
+        //             </li>
+        //         </div>
+        //     })}
+        // </div>
     );
 }
 
