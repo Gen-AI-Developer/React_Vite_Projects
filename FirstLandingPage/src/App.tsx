654d24523030188715d957e131cb153c2d9245ba
Syed Safdar Ali Shah <116350058+Gen-AI-Developer@users.noreply.github.com>
2024-05-31 22:02:02 +0500

    anotherProjectUpdated

---
+++ FirstLandingPage/src/App.tsx
@@ -0,0 +1,18 @@
+
+import './index.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <div>
+          <h1 className='font-semibold text-5xl bg-zinc-600'>Vite in React</h1>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
