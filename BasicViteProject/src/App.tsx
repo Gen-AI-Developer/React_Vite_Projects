0b7afbbcce946db6e9284b50e5ec6da4ff5f6fc9
Syed Safdar Ali Shah <116350058+Gen-AI-Developer@users.noreply.github.com>
2024-05-31 15:23:40 +0500

    Initial

---
+++ BasicViteProject/src/App.tsx
@@ -0,0 +1,27 @@
+
+import './App.css'
+
+function App() {
+  const user = {
+    name: 'Hedy Lamarr',
+    imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+    imageSize: 90,
+  };
+
+  return (
+    <>
+      <h1>{user.name}</h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+      />
+    </>
+  )
+}
+
+export default App
