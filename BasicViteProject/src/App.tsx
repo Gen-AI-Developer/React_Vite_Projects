dc8c4154708458360ec0665b926b0a739d654db3
Syed Safdar Ali Shah <116350058+Gen-AI-Developer@users.noreply.github.com>
2024-05-31 16:41:42 +0500

    conditionalRendering

--- BasicViteProject/src/App.tsx
+++ BasicViteProject/src/App.tsx
@@ -1,23 +1,23 @@
 
 import './App.css'
 import AdminPanel from './components/AdminPanel'
-import LoginForm from './components/LoginForm'
+// import LoginForm from './components/LoginForm'
 function App() {
   // const user = {
   //   name: 'Safdar',
   //   imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
   //   imageSize: 90,
   // };
-  let content;
-  if (true) {
-    content = <AdminPanel />;
-  } else {
-    content = <LoginForm />;
-  }
+  // let content;
+  // if (true) {
+  //   content = <AdminPanel />;
+  // } else {
+  //   content = <LoginForm />;
+  // }
 
   return (
     <>
-      {content}
+      {true && <AdminPanel />}
     </>
   )
 }
--- BasicViteProject/src/main.tsx
+++ BasicViteProject/src/main.tsx
@@ -1,15 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import App from './App.tsx'
+import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    {/* <App /> */}
-    <html>
-      <div>
-        <h1>This is Web Page</h1>
-      </div>
-    </html>
+    <App />
   </React.StrictMode>,
 )
